389e5a5bdf329c5b1635a96dff91009af348d964
deokisys <sjb378@me.com>
2024-12-05 15:54:59 +0900

    [feat] 헤드 레이아웃 구성
    헤드레이아웃의 로고와 로그인/가입 버튼 위치를 구성
    좌우로 떨어져서 구성되도록 구성
    init은 프리티어 적용

--- src/components/common/Layout/HeadLayout.tsx
+++ src/components/common/Layout/HeadLayout.tsx
@@ -1,19 +1,33 @@
+import styled from 'styled-components';
 import Button from '../Button/Button';
+
+const Head = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 16px;
+
+  border-bottom: 1px solid #ccc;
+`;
+const LoginBox = styled.div`
+  display: flex;
+  gap: 4px;
+`;
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 function HeadLayout(props: LayoutProps) {
   return (
-    <div>
-      <div>
+    <>
+      <Head>
         <div>로고</div>
-        <div>
-          <Button label="로그인" />
-        </div>
-      </div>
+        <LoginBox>
+          <Button label="로그인" variant="white" />
+          <Button label="회원가입" />
+        </LoginBox>
+      </Head>
       <div>{props.children}</div>
-    </div>
+    </>
   );
 }
 export default HeadLayout;
